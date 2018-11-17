649d4288d2482400b77add9b6fed6734a51b544b
keegandonley <keegan.donley@gmail.com>
2018-11-17 14:44:25 -0800

    loading flow

--- src/screens/Landing/components.js
+++ src/screens/Landing/components.js
@@ -6,14 +6,68 @@ export const Wrapper = styled.div`
 	display: flex;
 	align-items: center;
 	font-size: 2em;
-
-	& div {
-		margin: 0 auto;
-	}
 `;	
 
 export const StarContainer = styled.canvas`
 	position: absolute;
 	width: 100vw;
 	height: 100vh;
+`;
+
+export const LoadingStar = styled.div`
+	@keyframes fys-load {
+		0% {
+			transform: scale(0.5);
+		}
+		50% {
+			transform: scale(1);
+		}
+		100% {
+			transform: scale(0.5);
+		}
+	}
+	margin: 0 auto;
+	width: 200px;
+	display: flex;
+
+	& svg {
+		margin: 0 auto;
+		font-size: 2em;
+		animation: fys-load 2s infinite;
+	}
+	padding-bottom: 20px;
+	color: #FFFFAA;
+`;
+
+export const LoaderWrapper = styled.div`
+	margin: 0 auto;
+	width: 90%;
+	max-width: 500px;
+	height: 6px;
+	border: 1px solid #FFFFAA;
+	border-radius: 8px;
+	opacity: ${props => (props.visible ? 1 : 0)};
+	transition: opacity 1s;
+`;
+
+export const LoaderInner = styled.div`
+	width: ${props => props.perc || 0}%;
+	height: 100%;
+	background-color: #FFFFAA;
+	border-radius: 8px;
+	transition: width 2s;
+`;
+
+export const LoadingText = styled.div`
+	text-align: center;
+	padding-top: 8px;
+	font-size: 0.6em;
+	opacity: ${props => (props.visible ? 1 : 0)};
+	transition: opacity 1s;
+	color: #FFFFAA;
+`;
+
+export const LoadingData = styled.div`
+	width: 100%;
+	margin: 0 auto;
 `;
--- src/screens/Landing/index.js
+++ src/screens/Landing/index.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react';
-import { Wrapper, StarContainer } from './components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/pro-solid-svg-icons';
+import { Redirect } from 'react-router-dom';
+import { Wrapper, StarContainer, LoadingStar, LoaderWrapper, LoaderInner, LoadingText, LoadingData } from './components';
 import Starjs from './stargen';
 
 export default class Landing extends Component {
+	state = {
+		loading: true,
+		loadingPerc: 0,
+		loadingText: 'preparing'
+	}
 	componentDidMount() {
 		const stars = new Starjs({ id: 'myStars' });
-  		stars.init();
+		stars.init();
+		setTimeout(() => {
+			this.setState({ loadingPerc: 10 });
+		}, 1000);
+		setTimeout(() => {
+			this.setState({ loadingPerc: 25, loadingText: 'fetching map data' });
+		}, 2000);
+		setTimeout(() => {
+			this.setState({ loadingPerc: 50, loadingText: 'fetching map images' });
+		}, 3000);
+		setTimeout(() => {
+			this.setState({ loadingPerc: 75, loadingText: 'logging in securely' });
+		}, 4000);
+		setTimeout(() => {
+			this.setState({ loadingPerc: 100, loadingText: 'redirecting' });
+		}, 5000);
+		setTimeout(() => {
+			this.setState({ loading: false });
+		}, 7000);
 	}
 
 	render() {
+		const { loading, loadingPerc, loadingText } = this.state;
+		if (!loading) {
+			return <Redirect to="/map" />;
+		}
 		return (
 			<Wrapper>
 				<StarContainer id="myStars"></StarContainer>
-				<div>
-					hello world
-				</div>
+				<LoadingData>
+					<LoadingStar>
+						<FontAwesomeIcon icon={faStar} />
+					</LoadingStar>
+					<LoaderWrapper visible={loadingPerc > 0}>
+						<LoaderInner perc={loadingPerc}/>
+					</LoaderWrapper>
+					<LoadingText visible={loadingPerc > 0}>{loadingText}...</LoadingText>
+				</LoadingData>
 			</Wrapper>
 		);
 	}
