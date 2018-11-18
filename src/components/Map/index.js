54477c28caa59c1f58fa1bd63a57f38c7933ee51
keegandonley <keegan.donley@gmail.com>
2018-11-18 00:51:23 -0800

    map bounds

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { Map, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
+import { Map, Marker, Popup, GeoJSON } from 'react-leaflet';
 import L from 'leaflet';
 import MapboxLayer from '../MapBox';
 import ISSIconImage from './ISSicon.png';
@@ -22,12 +22,14 @@ export default class MapComponent extends Component {
       shadowUrl: null,
       shadowSize: null,
       shadowAnchor: null
-  });
+    });
+
     return (
-      <Map center={currentPosition} zoom={this.state.zoom}>
+      <Map center={currentPosition} zoom={this.state.zoom} maxBounds={[[-90,-180], [90,180]]} minZoom={3}>
         <MapboxLayer
           accessToken={process.env.REACT_APP_MAPBOX_TOKEN}
           style='mapbox://styles/mauricebecnel400/cjomdc81d1tch2sp3zcedcmqv'
+          renderWorldCopies={false}
         >
         </MapboxLayer>
         <GeoJSON
