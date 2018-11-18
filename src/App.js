8b850e43c799c9ef817ffab56ff3311edb396dba
keegandonley <keegan.donley@gmail.com>
2018-11-18 03:12:22 -0800

    router fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import { Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import * as Screens from './screens';
 
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Route path="/" exact component={Screens.Landing} />
-        <Route path='/map' component={Screens.Map}/>
-      </React.Fragment>
+      <Router>
+        <div>
+          <Route path="/" exact component={Screens.Landing} />
+          <Route path='/map' component={Screens.Map}/>
+        </div>
+      </Router>
     );
   }
 }
