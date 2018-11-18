edd29e566b229069320410b7106568f07dd0b947
Maurice Becnel <mauricebenel400@gmail.com>
2018-11-17 21:34:19 -0800

    MapBox Tiles

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,26 +1,30 @@
 
 import React, { Component } from 'react';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-  
+import { Map, Marker, Popup } from 'react-leaflet';
+import MapboxLayer from '../MapBox';
+
+
 export default class MapComponent extends Component {
   state = {
     zoom: 13,
   }
 
   render() {
+    console.log(process.env.REACT_APP_MAPBOX_TOKEN);
     const currentPosition = [this.props.currentLat, this.props.currentLng]
     return (
       <Map center={currentPosition} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
-        />
-        <Marker position={currentPosition}>
-          <Popup>
-            You are Here
-            <br/>
-          </Popup>
-        </Marker>
+        <MapboxLayer
+          accessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+          style='mapbox://styles/mauricebecnel400/cjomdc81d1tch2sp3zcedcmqv'
+        >
+          <Marker position={currentPosition}>
+            <Popup>
+              You are Here
+              <br/>
+            </Popup>
+          </Marker>
+        </MapboxLayer>
       </Map>
     )
   }
